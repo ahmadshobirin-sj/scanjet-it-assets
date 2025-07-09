568ad45a251ec5dbece2c476c076c2b30e2fa529
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-09 14:46:43 +0700

    feat: update user creation logic in NewMicrosoft365SignInListener

--- app/Listeners/NewMicrosoft365SignInListener.php
+++ app/Listeners/NewMicrosoft365SignInListener.php
@@ -11,11 +11,17 @@ class NewMicrosoft365SignInListener
 {
     public function handle(object $event): void
     {
-        $user = User::firstOrCreate([
+        $user = User::updateOrCreate([
             'email' => $event->token['info']['mail'],
         ], [
-            'id' => (string) Str::uuid(),
             'name' => $event->token['info']['displayName'],
+            'given_name' => $event->token['info']['givenName'] ?? null,
+            'surname' => $event->token['info']['surname'] ?? null,
+            'user_principal_name' => $event->token['info']['userPrincipalName'] ?? null,
+            'business_phones' => $event->token['info']['businessPhones'] ?? [],
+            'mobile_phone' => $event->token['info']['mobilePhone'] ?? null,
+            'job_title' => $event->token['info']['jobTitle'] ?? null,
+            'office_location' => $event->token['info']['officeLocation'] ?? null,
             'email' => $event->token['info']['mail'] ?? $event->token['info']['userPrincipalName'],
         ]);
 
