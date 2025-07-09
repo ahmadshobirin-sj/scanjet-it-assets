865d63f0e344cebd1f50be64d448a91b628c81d8
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-09 14:55:24 +0700

    feat: setup permission & role

---
+++ app/Models/Role.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Permission\Models\Role as ModelsRole;
+
+class Role extends ModelsRole
+{
+    use HasFactory;
+    use HasUuids;
+
+    protected $with = ['permissions'];
+
+    public function getColor(): string
+    {
+        return match ($this->name) {
+            'Super Admin' => 'primary',
+            'Admin' => 'success',
+            'Inactive' => 'secondary',
+            default => 'warning',
+        };
+    }
+}
