dfbe9fa77c9d869ab7c002774f76df62ce643eb5
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-09 15:03:53 +0700

    feat: add UserRoleStyle class for managing user role intents

---
+++ resources/js/lib/userRoleStyle.ts
@@ -0,0 +1,16 @@
+import { UserRole } from "@/types/model";
+
+export class UserRoleStyle {
+    static getIntent(role: UserRole): string {
+        switch (role) {
+            case 'Admin':
+                return 'info';
+            case 'Super Admin':
+                return 'primary';
+            case 'Inactive':
+                return 'secondary';
+            default:
+                return 'secondary';
+        }
+    }
+}
