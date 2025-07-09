993167682908dbe42ab6a3991a75cce1da3c57e1
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-09 14:55:41 +0700

    fix: update middleware syntax for guest and authenticated routes

--- routes/auth.php
+++ routes/auth.php
@@ -4,13 +4,13 @@ use App\Http\Controllers\Auth\AuthController;
 use Dcblogdev\MsGraph\Facades\MsGraph;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
+Route::middleware(['guest'])->group(function () {
     Route::get('login', [AuthController::class, 'login'])
         ->name('login');
     Route::get('auth/callback', [AuthController::class, 'connect'])->name('connect');
 });
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['MsGraphAuthenticated'])->group(function () {
     Route::post('logout', [AuthController::class, 'logout'])
         ->name('logout');
 });
