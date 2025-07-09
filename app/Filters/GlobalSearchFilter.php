d91b6a585ec4511185da368f0249b17f07fcaed2
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-09 14:44:57 +0700

    feat: implement GlobalSearchFilter for dynamic querying of model attributes

---
+++ app/Filters/GlobalSearchFilter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filters;
+
+use Spatie\QueryBuilder\Filters\Filter;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Schema;
+
+class GlobalSearchFilter implements Filter
+{
+    protected array $ignored;
+
+    public function __construct(array $ignored = [])
+    {
+        $this->ignored = $ignored;
+    }
+
+    public function __invoke(Builder $query, $value, string $property): Builder
+    {
+        if (empty($value)) {
+            return $query;
+        }
+
+        $model = $query->getModel();
+        $table = $model->getTable();
+        $columns = Schema::getColumnListing($table);
+        $searchable = array_filter($columns, function ($column) use ($table) {
+            $type = Schema::getColumnType($table, $column);
+            return in_array($type, ['string', 'text', 'varchar']);
+        });
+
+        $searchable = array_diff($searchable, $this->ignored);
+
+        $searchValue = addslashes($value); // Basic sanitization
+
+        return $query->where(function ($query) use ($searchValue, $searchable) {
+            foreach ($searchable as $column) {
+                $query->orWhere($column, 'LIKE', "%{$searchValue}%");
+            }
+        });
+    }
+}
