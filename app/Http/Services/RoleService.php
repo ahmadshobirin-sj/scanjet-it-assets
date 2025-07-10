12f2b110a85b9a8681694d77312e14fe7291ea9e
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-10 16:41:44 +0700

    feat: implement RoleService methods for CRUD operations and permissions management

--- app/Http/Services/RoleService.php
+++ app/Http/Services/RoleService.php
@@ -6,6 +6,9 @@ use App\Http\Filters\GlobalSearchFilter;
 use App\Http\Sorts\Role\TotalPermissionsSort;
 use App\Models\Role;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\AllowedSort;
 use Spatie\QueryBuilder\QueryBuilder;
@@ -20,8 +23,50 @@ class RoleService
                 AllowedFilter::custom('search', new GlobalSearchFilter()),
             ])
             ->allowedSorts(['name', AllowedSort::custom('total_permissions', new TotalPermissionsSort), 'created_at'])
-            ->defaultSort(['name'])
+            ->defaultSort(['-created_at'])
             ->paginate($request->input('per_page', 10))
             ->appends($request->query());
+    }
+
+    public function getById(string $id): Role
+    {
+        return QueryBuilder::for(Role::class)
+            ->withCount('permissions')
+            ->findOrFail($id);
+    }
+
+    public function create(array $data): Role
+    {
+        return DB::transaction(function () use ($data) {
+            $role = Role::create(Arr::only($data, $this->attributes()));
+
+            $role->syncPermissions($data['permissions']);
+
+            return $role;
+        });
+    }
+
+    public function attributes(): array
+    {
+        return Schema::getColumnListing((new Role)->getTable());
+    }
+
+    public function update(Role $role, array $data): Role
+    {
+        return DB::transaction(function () use ($role, $data) {
+            $role->update(Arr::only($data, $this->attributes()));
+
+            $role->syncPermissions($data['permissions']);
+
+            return $role;
+        });
+    }
+
+    public function delete(Role $role): void
+    {
+        DB::transaction(function () use ($role) {
+            $role->permissions()->detach();
+            $role->delete();
+        });
     }
 }
