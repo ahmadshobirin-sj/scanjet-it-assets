81ca9f1a214132ad33e24fdbde4b8720acf07a18
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-11 11:08:12 +0700

    refactor: streamline AppSidebar to use dynamic menu items from useMenu hook

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -2,40 +2,24 @@ import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
 import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
-import { type NavItem } from '@/types';
-import { Link } from '@inertiajs/react';
+import { SharedData, type NavItem } from '@/types';
+import { Link, usePage } from '@inertiajs/react';
 import { LayoutGrid, ShieldUser, Users } from 'lucide-react';
 import AppLogo from './app-logo';
-
-const mainNavItems: NavItem[] = [
-    {
-        title: 'Dashboard',
-        href: '/',
-        icon: LayoutGrid,
-    },
-    {
-        title: 'Users',
-        href: '/user',
-        icon: Users,
-    },
-    {
-        title: 'Roles',
-        href: '/role',
-        icon: ShieldUser,
-    },
-];
-
-const footerNavItems: NavItem[] = [];
+import { useMenu } from '@/hooks/use-menu';
 
 export function AppSidebar() {
+    const { mainNavItems, footerNavItems } = useMenu();
+    const page = usePage<SharedData>();
+
     return (
-        <Sidebar collapsible="offcanvas" variant="sidebar">
+        <Sidebar collapsible="offcanvas" variant="inset">
             <SidebarHeader>
                 <SidebarMenu>
                     <SidebarMenuItem>
-                        <SidebarMenuButton size="lg" asChild>
-                            <Link href="/dashboard" prefetch>
-                                <AppLogo />
+                        <SidebarMenuButton size="lg" asChild className="bg-sidebar-accent text-sidebar-accent-foreground hover:bg-sidebar-accent/90">
+                            <Link href={route('dashboard')} prefetch>
+                                <AppLogo name={page.props.name} />
                             </Link>
                         </SidebarMenuButton>
                     </SidebarMenuItem>
