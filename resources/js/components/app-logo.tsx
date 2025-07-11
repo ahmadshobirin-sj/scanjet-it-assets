fda1103a7dab53b990634bfb2c9e17c611d3f8a1
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-11 10:43:30 +0700

    feat: update AppLogo component to accept dynamic name and logo props

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,13 +1,12 @@
+import { usePage } from '@inertiajs/react';
 import AppLogoIcon from './app-logo-icon';
+import { SharedData } from '@/types';
 
-export default function AppLogo() {
+export default function AppLogo({ name, logo }: { name?: string; logo?: string }) {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
-            </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">SCITA</span>
+                <span className="mb-0.5 truncate leading-tight font-semibold">{name}</span>
             </div>
         </>
     );
