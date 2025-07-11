e8701eb8b5eeff5e4c4eec2244f5fe89fc1941ff
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-11 11:10:03 +0700

    feat: add filter functions for NavItems and NavGroups based on user permissions

---
+++ resources/js/lib/menu.tsx
@@ -0,0 +1,32 @@
+import { NavGroup, NavItem } from "@/types";
+
+export function filterItems(items: NavItem[], permissions?: string[]): NavItem[] {
+    return items
+        .map((item) => {
+            if (item.items) {
+                const filteredChildren = filterItems(item.items, permissions);
+                return {
+                    ...item,
+                    items: filteredChildren.length ? filteredChildren : undefined,
+                };
+            }
+            return item;
+        })
+        .filter((item) => {
+            const itemPermission = item.meta?.permission;
+            return (
+                !item.hidden &&
+                (!item.items || item.items.length > 0) &&
+                (permissions && itemPermission ? permissions.includes(itemPermission) : true)
+            );
+        });
+}
+
+export function filterNavGroups(groups: NavGroup[], permissions?: string[]): NavGroup[] {
+    return groups
+        .map((group) => {
+            const filteredItems = filterItems(group.items, permissions);
+            return { ...group, items: filteredItems };
+        })
+        .filter((group) => group.items.length > 0);
+}
