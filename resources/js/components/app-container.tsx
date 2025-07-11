391674ddf791278197543a235f37949fa751d0a0
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-11 11:07:27 +0700

    feat: enhance AppContainer to support dynamic padding based on withPadding prop

--- resources/js/components/app-container.tsx
+++ resources/js/components/app-container.tsx
@@ -1,9 +1,10 @@
 import { cn } from '@/lib/utils'
 import React from 'react'
 
-function AppContainer({ children, className, ...props }: { children: React.ReactNode } & React.HTMLAttributes<HTMLDivElement>) {
+function AppContainer({ children, className, withPadding = true, ...props }: { children: React.ReactNode, withPadding?: boolean } & React.HTMLAttributes<HTMLDivElement>) {
     return (
-        <div className={cn('p-4', className)} {...props}>
+        <div className={cn('w-full mx-auto',
+            withPadding ? 'p-4 sm:p-6 md:p-8 lg:p-10' : '', className)} {...props}>
             {children}
         </div>
     )
