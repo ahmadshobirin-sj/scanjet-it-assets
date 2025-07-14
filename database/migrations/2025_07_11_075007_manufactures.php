e293ffc40a688d880a59dc06470077ce63f1c21d
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-14 13:34:12 +0700

    feat: create manufactures migration with necessary fields

---
+++ database/migrations/2025_07_11_075007_manufactures.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("manufactures", function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string("name")->unique();
+            $table->string("address")->nullable();
+            $table->string("phone")->nullable();
+            $table->string("email")->nullable();
+            $table->string("website")->nullable();
+            $table->string("contact_person_name")->nullable();
+            $table->string("contact_person_phone")->nullable();
+            $table->string("contact_person_email")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('manufactures');
+    }
+};
