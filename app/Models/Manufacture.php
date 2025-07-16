48a64a6265acbcc2102ce0d35eb0af83abc6a94c
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-16 15:35:03 +0700

    fix: remove unused HasFormattedTimestamp trait from Manufacture model

--- app/Models/Manufacture.php
+++ app/Models/Manufacture.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\HasFormattedTimestamp;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -11,7 +10,6 @@ class Manufacture extends Model
 {
     use HasFactory;
     use HasUuids;
-    use HasFormattedTimestamp;
 
     protected $fillable = [
         'name',
