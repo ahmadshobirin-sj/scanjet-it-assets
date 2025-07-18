a3542aeedb202e6cfcaa988133c470850ce18b94
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-18 15:49:33 +0700

    feat: create migration for assets table with necessary fields and relationships

---
+++ database/migrations/2025_07_17_045944_assets.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("assets", function (Blueprint $table) {
+            $table->uuid("id")->primary();
+            $table->string("name");
+            $table->string("location")->nullable();
+            $table->string("serial_number")->unique()->nullable();
+            $table->date("warranty_expired")->nullable();
+            $table->date("last_maintenance")->nullable();
+            $table->text("note")->nullable();
+            $table->date("purchase_date")->nullable();
+
+            $table->uuid('category_id');
+            $table->foreign('category_id')->references('id')->on('asset_categories')->onDelete('restrict');
+
+            $table->uuid('manufacture_id');
+            $table->foreign("manufacture_id")->references('id')->on("manufactures")->onDelete("restrict");
+
+            $table->enum("status", ["available", "assigned", "maintenance", "lost", "scrapped"])->default("available");
+
+            $table->string('assigned_user_id')->nullable();
+            $table->timestamp('assigned_at')->nullable();
+
+            $table->string("reference_link")->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assets');
+    }
+};
