316da882d9197b68055c77bb6ba171a8f29bb5b8
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-18 15:49:11 +0700

    feat: add MySQL CRM database connection configuration

--- config/database.php
+++ config/database.php
@@ -62,6 +62,26 @@ return [
             ]) : [],
         ],
 
+        'mysql_crm' => [
+            'driver' => 'mysql',
+            'url' => env('CRM_DB_URL'),
+            'host' => env('CRM_DB_HOST', '127.0.0.1'),
+            'port' => env('CRM_DB_PORT', '3306'),
+            'database' => env('CRM_DB_DATABASE', 'laravel'),
+            'username' => env('CRM_DB_USERNAME', 'root'),
+            'password' => env('CRM_DB_PASSWORD', ''),
+            'unix_socket' => env('CRM_DB_SOCKET', ''),
+            'charset' => env('CRM_DB_CHARSET', 'utf8mb4'),
+            'collation' => env('CRM_DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
@@ -147,7 +167,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
