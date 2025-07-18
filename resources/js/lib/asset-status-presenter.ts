2fd23ae699fad17ec5f3b96a562c9e57b4548f60
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-18 15:55:24 +0700

    feat: implement AssetStatusPresenter class for asset status management

---
+++ resources/js/lib/asset-status-presenter.ts
@@ -0,0 +1,41 @@
+import { AssetStatus } from "@/types/model";
+
+export class AssetStatusPresenter  {
+    static getIntent(status: AssetStatus): string {
+        switch (status) {
+            case 'available':
+                return 'primary';
+            case 'assigned':
+                return 'success';
+            case 'maintenance':
+                return 'info';
+            case 'lost':
+                return 'destructive';
+            case 'scrapped':
+                return 'destructive';
+            default:
+                return 'secondary';
+        }
+    }
+
+    static getLabel(status: AssetStatus): string {
+        switch (status) {
+            case 'available':
+                return 'Available';
+            case 'assigned':
+                return 'Assigned';
+            case 'maintenance':
+                return 'Maintenance';
+            case 'lost':
+                return 'Lost';
+            case 'scrapped':
+                return 'Scrapped';
+            default:
+                return 'Unknown';
+        }
+    }
+
+    static getValue(status: AssetStatus): string {
+        return status;
+    }
+}
