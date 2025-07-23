d2203002b3d495637b3e78127b3b278806ed5bc2
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-23 16:21:03 +0700

    style: adjust indentation and formatting in animation and SkeletonDataGrid components refactor: remove unnecessary 'use client' directive from MultipleSelector component

--- resources/js/components/skeleton/skeleton-data-grid.tsx
+++ resources/js/components/skeleton/skeleton-data-grid.tsx
@@ -8,41 +8,36 @@ type SkeletonDataGridProps = {
 
 const SkeletonDataGrid: FC<SkeletonDataGridProps> = ({ rows = 5, columns = 5 }) => {
     return (
-        <div className="space-y-4 w-full rounded-md p-4">
+        <div className="w-full space-y-4 rounded-md p-4">
             {/* Global Filter */}
-            <div className="flex justify-between items-center">
+            <div className="flex items-center justify-between">
                 <Skeleton className="h-7 w-[240px] rounded-sm" />
             </div>
 
             {/* Table */}
-            <div className="w-full border rounded-md overflow-hidden">
+            <div className="w-full overflow-hidden rounded-md border">
                 {/* Header */}
-                <div className="flex items-center px-4 py-2 border-b">
+                <div className="flex items-center border-b px-4 py-2">
                     {Array.from({ length: columns }).map((_, index) => (
-                        <Skeleton key={index} className="h-4 w-full mx-2" />
+                        <Skeleton key={index} className="mx-2 h-4 w-full" />
                     ))}
                 </div>
 
                 {/* Rows */}
                 {Array.from({ length: rows }).map((_, rowIndex) => (
-                    <div
-                        key={rowIndex}
-                        className="flex items-center px-4 py-3 border-b last:border-none"
-                    >
+                    <div key={rowIndex} className="flex items-center border-b px-4 py-3 last:border-none">
                         {Array.from({ length: columns }).map((_, colIndex) => (
-                            <Skeleton key={colIndex} className="h-4 w-full mx-2" />
+                            <Skeleton key={colIndex} className="mx-2 h-4 w-full" />
                         ))}
                     </div>
                 ))}
             </div>
 
             {/* Pagination */}
-            <div className="flex justify-end items-center gap-2">
-                {
-                    Array.from({ length: 3 }).map((_, index) => (
-                        <Skeleton key={index} className="h-6 w-6 rounded-sm" />
-                    ))
-                }
+            <div className="flex items-center justify-end gap-2">
+                {Array.from({ length: 3 }).map((_, index) => (
+                    <Skeleton key={index} className="h-6 w-6 rounded-sm" />
+                ))}
             </div>
         </div>
     );
