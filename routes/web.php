19819642a65b7a49da47096762cb21d4780302e8
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-24 14:16:54 +0700

    fix: standardize require statements formatting in web.php

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,12 @@ Route::middleware(['MsGraphAuthenticated'])->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 });
 
-require __DIR__.'/asset.php';
-require __DIR__.'/manufacture.php';
-require __DIR__.'/asset_category.php';
-require __DIR__.'/settings.php';
-require __DIR__.'/user.php';
-require __DIR__.'/role.php';
-require __DIR__.'/auth.php';
+require __DIR__ . '/asset.php';
+require __DIR__ . '/manufacture.php';
+require __DIR__ . '/asset_category.php';
+require __DIR__ . '/settings.php';
+require __DIR__ . '/user.php';
+require __DIR__ . '/role.php';
+require __DIR__ . '/auth.php';
+
+require __DIR__ . '/test.php';
