2ba9576a76c50e84a96eeb90a69569716d81a1d3
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-24 14:21:30 +0700

    feat: add test routes for data grid and multiple selector functionality

---
+++ routes/test.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\Route;
+
+// Test routes
+Route::get('/test/data-grid', [TestController::class, 'dataGrid'])->name('test.data-grid');
+Route::get('/test/simple-data-grid', function () {
+    return inertia('simple-data-grid-test');
+})->name('test.simple-data-grid');
+Route::get('/test/multiple-selector', [TestController::class, 'multipleSelector'])->name('test.multiple-selector');
