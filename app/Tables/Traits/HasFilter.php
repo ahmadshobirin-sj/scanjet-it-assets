bbdb446834b45957a47048b999e37d656ab28adf
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-24 14:27:32 +0700

    refactor: simplify filterResolver method by removing global search logic

--- app/Tables/Traits/HasFilter.php
+++ app/Tables/Traits/HasFilter.php
@@ -2,81 +2,22 @@
 
 namespace App\Tables\Traits;
 
-use App\Tables\Columns\Column;
-use Illuminate\Support\Facades\DB;
-use Spatie\QueryBuilder\AllowedFilter;
+use App\Tables\FilterColumns\FilterColumn;
 
 trait HasFilter
 {
     /**
      * Return filters and search filters for QueryBuilder.
      */
-    /**
-     * Return filters including global search filter.
-     */
-    public function filters(): array
+    public function filterResolver(): array
     {
-        $columns = collect($this->columns());
-
-        $globalSearchableColumns = $columns
-            ->filter(fn (Column $col) => $col->isGlobalSearchable())
-            ->map(fn (Column $col) => $col->getName())
-            ->values()
-            ->all();
-
-        $globalSearch = AllowedFilter::callback('search', function ($query, $value) use ($globalSearchableColumns) {
-            static $processedColumns = null;
-
-            if ($processedColumns === null) {
-                $processedColumns = [
-                    'direct' => [],
-                    'relations' => [],
-                ];
-
-                foreach ($globalSearchableColumns as $column) {
-                    if (strpos($column, '.') !== false) {
-                        $parts = explode('.', $column);
-                        $relationColumn = array_pop($parts);
-                        $relationPath = implode('.', $parts);
-
-                        if (! isset($processedColumns['relations'][$relationPath])) {
-                            $processedColumns['relations'][$relationPath] = [];
-                        }
-                        $processedColumns['relations'][$relationPath][] = $relationColumn;
-                    } else {
-                        $processedColumns['direct'][] = $column;
-                    }
-                }
-            }
-
-            $query->where(function ($query) use ($value, $processedColumns) {
-                // Search direct columns
-                foreach ($processedColumns['direct'] as $column) {
-                    $query->orWhere($column, 'like', "%{$value}%");
-                }
-
-                // Search relationship columns
-                foreach ($processedColumns['relations'] as $relationPath => $columns) {
-                    $query->orWhereHas($relationPath, function ($relationQuery) use ($columns, $value) {
-                        $relationQuery->select(DB::raw('1')); // Minimal select untuk EXISTS
-
-                        $relationQuery->where(function ($whereQuery) use ($columns, $value) {
-                            foreach ($columns as $column) {
-                                $whereQuery->orWhere($column, 'like', "%{$value}%");
-                            }
-                        });
-                    });
-                }
-            });
-        });
-
-        $columnFilters = $columns
-            ->filter(fn (Column $col) => $col->getFilterType() !== null)
-            ->map(fn (Column $col) => $col->getAllowedFilter())
+        $filters = collect($this->filters());
+        $columnFilters = $filters
+            ->filter(fn (FilterColumn $col) => $col->getFilterType() !== null)
+            ->map(fn (FilterColumn $col) => $col->getAllowedFilter())
             ->values();
 
-        return collect([$globalSearch])
-            ->merge($columnFilters)
+        return collect($columnFilters)
             ->toArray();
     }
 
