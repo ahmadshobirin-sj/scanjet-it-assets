12d924f45f222413473bf5e4d18659e964aeedca
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-24 14:28:00 +0700

    refactor: remove filterable property from columns in AssetTable

--- app/Tables/AssetTable.php
+++ app/Tables/AssetTable.php
@@ -12,47 +12,38 @@ class AssetTable extends Table
         return [
             Column::make('name')
                 ->sortable()
-                ->filterable()
                 ->globallySearchable()
                 ->toggleable(),
             Column::make('category.name')
                 ->sortable('custom', new NestedRelationSort)
-                ->filterable()
                 ->globallySearchable()
                 ->toggleable(),
             Column::make('manufacture.name')
                 ->sortable('custom', new NestedRelationSort)
-                ->filterable()
                 ->globallySearchable()
                 ->toggleable(),
             Column::make('serial_number')
                 ->sortable()
-                ->filterable()
                 ->globallySearchable()
                 ->toggleable(),
             Column::make('location')
                 ->sortable()
-                ->filterable()
                 ->globallySearchable()
                 ->toggleable(),
             Column::make('warranty_expired')
                 ->sortable()
-                ->filterable()
                 ->globallySearchable()
                 ->toggleable(),
             Column::make('last_maintenance')
                 ->sortable()
-                ->filterable()
                 ->globallySearchable()
                 ->toggleable(),
             Column::make('created_at')
                 ->sortable()
-                ->filterable('exact')
                 ->globallySearchable()
                 ->toggleable(),
             Column::make('updated_at')
                 ->sortable()
-                ->filterable('exact')
                 ->globallySearchable()
                 ->toggleable(),
         ];
