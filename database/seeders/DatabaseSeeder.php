aebaffb483519e20ef3f12a042472cd6039f9b30
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-28 08:52:14 +0700

    refactor: replace User factory call with seeder calls for improved database seeding structure

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,6 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(1000)->create();
+        $this->call([
+            UserSeeder::class,
+            AssetCategorySeeder::class,
+            ManufactureSeeder::class,
+            AssetSeeder::class,
+            AssetAssignmentSeeder::class,
+        ]);
     }
 }
