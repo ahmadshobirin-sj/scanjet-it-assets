e8432810c67c74e502a20714648dcad869acd1fd
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-28 08:46:16 +0700

    feat: add AssetCategorySeeder for seeding asset categories in the database

---
+++ database/seeders/AssetCategorySeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\AssetCategory;
+use Illuminate\Database\Seeder;
+
+class AssetCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        AssetCategory::factory()->count(20)->create();
+    }
+}
