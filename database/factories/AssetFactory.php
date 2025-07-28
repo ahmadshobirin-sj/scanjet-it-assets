05ef9a54050afdae4d9497220f8c0e21f2fe04af
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-28 08:47:03 +0700

    feat: add AssetFactory for generating asset data in tests

---
+++ database/factories/AssetFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Asset;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AssetFactory extends Factory
+{
+    protected $model = Asset::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word(),
+            'category_id' => \App\Models\AssetCategory::inRandomOrder()->first()->id,
+            'manufacture_id' => \App\Models\Manufacture::inRandomOrder()->first()->id,
+            'location' => $this->faker->city(),
+            'serial_number' => $this->faker->unique()->bothify('SN-####-####'),
+            'asset_tag' => $this->faker->unique()->bothify('AT-####-####'),
+            'warranty_expired' => $this->faker->date(),
+            'purchase_date' => $this->faker->date(),
+            'note' => $this->faker->sentence(),
+            'reference_link' => $this->faker->url(),
+            'status' => 'available',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
