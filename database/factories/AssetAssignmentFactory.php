8d537f4bf03864dbebfec3410f3009bd8a402085
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-28 08:47:26 +0700

    feat: add AssetAssignmentFactory for generating asset assignment data in tests

---
+++ database/factories/AssetAssignmentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AssetAssignment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AssetAssignmentFactory extends Factory
+{
+    protected $model = AssetAssignment::class;
+
+    public function definition()
+    {
+        return [
+            'asset_id' => \App\Models\Asset::inRandomOrder()->first()->id,
+            'assigned_user_id' => \App\Models\ExternalUser::inRandomOrder()->first()->id,
+            'user_id' => \App\Models\User::inRandomOrder()->first()->id,
+            'note' => $this->faker->sentence(),
+            'status' => $this->faker->randomElement(['assigned', 'returned']),
+            'condition' => $this->faker->randomElement(['ok', 'loss', 'damaged', 'malfunction', 'theft']),
+            'assigned_at' => $this->faker->dateTimeBetween('-1 years', 'now'),
+            'returned_at' => $this->faker->optional()->dateTimeBetween('assigned_at', 'now'),
+        ];
+    }
+}
