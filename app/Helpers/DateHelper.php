3bae83f5bdca6a0d15df6799bf21fde68ff1380d
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-29 19:36:42 +0700

    refactor: enhance DateHelper format method to include timezone and optional time formatting

--- app/Helpers/DateHelper.php
+++ app/Helpers/DateHelper.php
@@ -11,14 +11,23 @@ class DateHelper
      *
      * @param  string|\DateTimeInterface|null  $date
      * @param  string  $format
+     * @param  bool  $withTime
+     * @param  string  $timezone
      * @return string|null
      */
-    public static function format($date, $format = 'd M Y')
+    public static function format($date, string $format = 'd M Y', bool $withTime = true, string $timezone = 'UTC'): ?string
     {
         if (! $date) {
             return null;
         }
 
-        return Carbon::parse($date)->translatedFormat($format);
+        $carbon = Carbon::parse($date)->setTimezone($timezone);
+
+        // Tambahkan jam jika diminta
+        if ($withTime) {
+            $format .= ' H:i';
+        }
+
+        return $carbon->translatedFormat($format);
     }
 }
