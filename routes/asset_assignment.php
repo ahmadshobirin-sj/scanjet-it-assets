f6158a2d50d0fc5da63b618015484271bdd0dc89
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-29 19:46:10 +0700

    feat: add routes for asset assignment creation and assignment handling

--- routes/asset_assignment.php
+++ routes/asset_assignment.php
@@ -6,4 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('MsGraphAuthenticated')->prefix('asset-assignment')->group(function () {
     Route::get('/', [AssetAssignmentController::class, 'index'])
         ->name('asset-assignment.index');
+    Route::get('/create', [AssetAssignmentController::class, 'create'])
+        ->name('asset-assignment.create');
+    Route::post('/create', [AssetAssignmentController::class, 'assign'])
+        ->name('asset-assignment.assign');
 });
