8e36613ef5538d7c8dd6fd555515d7873f3bfc44
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-07-29 19:44:37 +0700

    fix: replace GroupForm with native form for improved HTML structure

--- resources/js/pages/asset-category/create.tsx
+++ resources/js/pages/asset-category/create.tsx
@@ -83,19 +83,21 @@ const AssetCategoryCreatePage = () => {
                     <SheetDescription>Create a new asset category to organize your assets effectively.</SheetDescription>
                 </SheetHeader>
                 <SheetBody className="px-4">
-                    <GroupForm onSubmit={onSubmit}>
-                        <GroupFormItem>
-                            <Label htmlFor="name">Name</Label>
-                            <Input id="name" autoComplete="off" value={data.name} onChange={handleChangeName} />
-                            {errors.name && <FormMessage error>{errors.name}</FormMessage>}
-                        </GroupFormItem>
-                        <GroupFormItem>
-                            <Label htmlFor="description">Description</Label>
-                            <Textarea id="description" autoComplete="off" value={data.description} onChange={handleChangeDescription} />
-                            {errors.description && <FormMessage error>{errors.description}</FormMessage>}
-                        </GroupFormItem>
-                        <input type="submit" hidden />
-                    </GroupForm>
+                    <form onSubmit={onSubmit}>
+                        <GroupForm>
+                            <GroupFormItem>
+                                <Label htmlFor="name">Name</Label>
+                                <Input id="name" autoComplete="off" value={data.name} onChange={handleChangeName} />
+                                {errors.name && <FormMessage error>{errors.name}</FormMessage>}
+                            </GroupFormItem>
+                            <GroupFormItem>
+                                <Label htmlFor="description">Description</Label>
+                                <Textarea id="description" autoComplete="off" value={data.description} onChange={handleChangeDescription} />
+                                {errors.description && <FormMessage error>{errors.description}</FormMessage>}
+                            </GroupFormItem>
+                            <input type="submit" hidden />
+                        </GroupForm>
+                    </form>
                 </SheetBody>
                 <SheetFooter>
                     <Button loading={processing} onClick={onButtonSubmit}>
