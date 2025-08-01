7719a134f59d77309387be28b40d378400692ce4
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-01 11:04:27 +0700

    feat: implement GenerateRefCode class for unique reference code generation

---
+++ app/Helpers/GenerateRefCode.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Support\Str;
+
+class GenerateRefCode
+{
+    /**
+     * Generate a unique reference code.
+     *
+     * @return string
+     */
+    public static function generate(): string
+    {
+        $prefix = 'sj-ref';
+        $randomString = Str::random(5);
+        $code = Str::substr(md5(now()), rand(0, 3), 5);
+        $uuid = Str::uuid7();
+        $codeUuid = Str::substr($uuid, Str::length($uuid) - 5, 5);
+
+        $final = Str::upper($prefix . '-' . $randomString . $code . $codeUuid);
+
+        return $final;
+    }
+}
