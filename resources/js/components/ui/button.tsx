676fcb2df651da5c68fd33e0ac3421b0effe0f29
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-01 11:09:39 +0700

    fix: adjust button padding and size for improved styling consistency

--- resources/js/components/ui/button.tsx
+++ resources/js/components/ui/button.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils"
 import { Loader2 } from "lucide-react"
 
 const buttonVariants = cva(
-    "inline-flex items-center justify-center cursor-pointer gap-2 whitespace-nowrap rounded-md font-medium transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
+    "inline-flex items-center justify-center cursor-pointer gap-2 whitespace-nowrap rounded-md transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
     {
         variants: {
             intent: {
@@ -24,7 +24,7 @@ const buttonVariants = cva(
                 ghost: "",
             },
             size: {
-                sm: "h-8 rounded-md px-3 text-sm",
+                sm: "h-8 rounded-md px-2 text-xs",
                 md: "h-9 px-4 py-2 text-sm", // default
                 lg: "h-10 px-6 text-base",
                 xl: "h-12 px-8 text-lg",
