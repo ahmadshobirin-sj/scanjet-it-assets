faccb9c691066076e1a5f53be84a2d480b54a1dc
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-01 11:08:02 +0700

    fix: update AssetAssignmentFactory to correct reference_code and notes fields

--- database/factories/AssetAssignmentFactory.php
+++ database/factories/AssetAssignmentFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Helpers\GenerateRefCode;
 use App\Models\AssetAssignment;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -12,10 +13,10 @@ class AssetAssignmentFactory extends Factory
     public function definition()
     {
         return [
-            'asset_id' => \App\Models\Asset::inRandomOrder()->first()->id,
+            'reference_code' => GenerateRefCode::generate(),
             'assigned_user_id' => \App\Models\ExternalUser::inRandomOrder()->first()->id,
             'user_id' => \App\Models\User::inRandomOrder()->first()->id,
-            'note' => $this->faker->sentence(),
+            'notes' => $this->faker->sentence(),
             'status' => $this->faker->randomElement(['assigned', 'returned']),
             'condition' => $this->faker->randomElement(['ok', 'loss', 'damaged', 'malfunction', 'theft']),
             'assigned_at' => $this->faker->dateTimeBetween('-1 years', 'now'),
