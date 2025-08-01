88b3dd1d71ad4e1c78694175c70da4610fc39595
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-01 11:09:10 +0700

    feat: integrate AppNotificationProvider and configure Echo for real-time notifications

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,19 +3,37 @@ import '@fontsource-variable/inter';
 import '../css/app.css';
 
 import { createInertiaApp } from '@inertiajs/react';
+import { configureEcho } from '@laravel/echo-react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+import AppNotificationProvider from './components/app-notification/app-notification-provider';
 import { initializeTheme } from './hooks/use-appearance';
+import { SharedData } from './types';
+
+configureEcho({
+    broadcaster: 'reverb',
+    key: import.meta.env.VITE_REVERB_APP_KEY,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
+});
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-createInertiaApp({
+createInertiaApp<SharedData>({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <>
+                <AppNotificationProvider {...props.initialPage} />
+                <App {...props} />
+            </>,
+        );
     },
     progress: {
         color: '#ffffff',
