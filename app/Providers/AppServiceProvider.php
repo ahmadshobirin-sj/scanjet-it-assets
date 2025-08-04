1c5d280282119fb52e509ce584d91903f213921d
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-04 14:59:25 +0700

    feat: enhance AppServiceProvider with rate limiting and destructive command prohibition

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,11 @@ namespace App\Providers;
 
 use App\Listeners\NewMicrosoft365SignInListener;
 use Dcblogdev\MsGraph\Events\NewMicrosoft365SignInEvent;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,7 +18,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
@@ -22,6 +29,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        DB::prohibitDestructiveCommands(app()->isProduction());
+
+        RateLimiter::for('webhook', function (Request $request) {
+            return Limit::perMinute(60)->by($request->ip());
+        });
+
         Event::listen(
             NewMicrosoft365SignInEvent::class,
             [NewMicrosoft365SignInListener::class, 'handle']
