a7c22d06301d569734e8fafa40c854ed6e89ad77
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-06 13:51:47 +0700

    refactor: update menu filtering logic to use filterItems for consistency

--- resources/js/hooks/use-menu.ts
+++ resources/js/hooks/use-menu.ts
@@ -1,19 +1,19 @@
-import { filterNavGroups } from '@/lib/menu';
+import { filterItems } from '@/lib/menu';
 import { footerNavItemsRaw, mainNavItemsRaw } from '@/menu';
 import { SharedData } from '@/types';
 import { usePage } from '@inertiajs/react';
 
 export const useMenu = () => {
     const page = usePage<SharedData>();
-    const mainNavItems = filterNavGroups(mainNavItemsRaw, page.props.auth.user.permissions);
-    const footerNavItems = filterNavGroups(footerNavItemsRaw, page.props.auth.user.permissions);
+
+    const mainNavItems = filterItems(mainNavItemsRaw, page.props.auth.user.permissions);
+    const footerNavItems = filterItems(footerNavItemsRaw, page.props.auth.user.permissions);
 
     const allNavItems = [...mainNavItems, ...footerNavItems];
-    const allNavItemsWithoutGroups = [...mainNavItems, ...footerNavItems].flatMap((group) => group.items);
+
     return {
         mainNavItems,
         footerNavItems,
         allNavItems,
-        allNavItemsWithoutGroups,
     };
 };
