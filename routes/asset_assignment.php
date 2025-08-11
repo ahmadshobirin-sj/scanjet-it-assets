2944b5a84c2b4e3c50d5f5cc72ca52d8597ccb5b
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-11 11:17:16 +0700

    refactor: update asset assignment routes for clarity and consistency

--- routes/asset_assignment.php
+++ routes/asset_assignment.php
@@ -1,15 +1,20 @@
 <?php
 
 use App\Http\Controllers\AssetAssignmentController;
+use App\Http\Controllers\AssetReturnController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('MsGraphAuthenticated')->prefix('asset-assignment')->group(function () {
     Route::get('/', [AssetAssignmentController::class, 'index'])
         ->name('asset-assignment.index');
-    Route::get('/create', [AssetAssignmentController::class, 'create'])
-        ->name('asset-assignment.create');
-    Route::post('/create', [AssetAssignmentController::class, 'assign'])
+    Route::get('/assign', [AssetAssignmentController::class, 'create'])
         ->name('asset-assignment.assign');
+    Route::post('/assign', [AssetAssignmentController::class, 'assign'])
+        ->name('asset-assignment.storeAssign');
+    Route::get('/return/{reference_code}', [AssetReturnController::class, 'create'])
+        ->name('asset-assignment.return');
+    Route::put('/return/{reference_code}/test', [AssetReturnController::class, 'store'])
+        ->name('asset-assignment.storeReturn');
 });
 
 Route::middleware('signed')->prefix('asset-assignment')->group(function () {
