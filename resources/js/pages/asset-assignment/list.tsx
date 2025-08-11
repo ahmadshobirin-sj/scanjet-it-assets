c5eba6e25ebdce53340fb68466cef79196d87a87
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-11 11:17:46 +0700

    refactor: rename assets state to assignments and update related logic for clarity

--- resources/js/pages/asset-assignment/list.tsx
+++ resources/js/pages/asset-assignment/list.tsx
@@ -10,14 +10,19 @@ import { AssetAssignment, ResponseCollection } from '@/types/model';
 import { TableServer } from '@/types/table';
 import { Page, router } from '@inertiajs/core';
 import { usePage, WhenVisible } from '@inertiajs/react';
-import { PackageMinus, PackagePlus } from 'lucide-react';
+import { PackagePlus } from 'lucide-react';
 import { useState } from 'react';
+import { columns as TableColumns } from './column';
 
 const AssetAssignmentListPage = () => {
     const { component } = usePage<SharedData>();
     const breadcrumbs = useBreadcrumb(component);
-    const [assets, setAsssets] = useState<ResponseCollection<AssetAssignment> | undefined>(undefined);
-    const { setTable, columns, tableState, tableStateServer } = useTableResolver('assets-assignment-table');
+    const [assignments, setAssignments] = useState<ResponseCollection<AssetAssignment> | undefined>(undefined);
+    const { setTable, columns, tableState, tableStateServer } = useTableResolver('assets-assignment-table', TableColumns);
+
+    const handleReturn = (row: AssetAssignment) => {
+        router.visit(route('asset-assignment.return', { reference_code: row.reference_code }));
+    };
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
@@ -27,10 +32,7 @@ const AssetAssignmentListPage = () => {
                     subtitle="View and manage asset assignments within the organization."
                     actions={
                         <>
-                            <Button intent="warning" leading={<PackageMinus />}>
-                                Return assets
-                            </Button>
-                            <Button intent="success" leading={<PackagePlus />} onClick={() => router.visit(route('asset-assignment.create'))}>
+                            <Button intent="success" leading={<PackagePlus />} onClick={() => router.visit(route('asset-assignment.assign'))}>
                                 Assign assets
                             </Button>
                         </>
@@ -50,7 +52,7 @@ const AssetAssignmentListPage = () => {
                             const typedData = data as Page<
                                 SharedData & { assetAssignments: ResponseCollection<AssetAssignment>; table: TableServer<AssetAssignment> }
                             >;
-                            setAsssets(typedData.props.assetAssignments);
+                            setAssignments(typedData.props.assetAssignments);
                             setTable(typedData.props.table);
                         },
                     }}
@@ -58,13 +60,14 @@ const AssetAssignmentListPage = () => {
                 >
                     <DataGrid
                         columns={columns}
-                        rows={assets?.data || []}
+                        rows={assignments?.data || []}
                         pageSizeOptions={[10, 25, 50]}
                         tableState={tableState}
                         serverSide
-                        rowCount={assets?.meta?.total || 0}
+                        rowCount={assignments?.meta?.total || 0}
                         rowId={(row) => (row.id ? row.id.toString() : '')}
                         enableRowSelection
+                        actionsRow={(row) => [...(!row.returned_at ? [{ name: 'Return', event: handleReturn }] : [])]}
                     />
                 </WhenVisible>
             </AppContainer>
