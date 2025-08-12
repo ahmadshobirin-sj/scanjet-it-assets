19d4df2a28812000b70523143061422803a4fba6
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-12 20:38:37 +0700

    refactor: streamline AssetAssignmentListPage by removing unused state and simplifying data fetching

--- resources/js/pages/asset-assignment/list.tsx
+++ resources/js/pages/asset-assignment/list.tsx
@@ -1,24 +1,26 @@
 import AppContainer from '@/components/app-container';
 import AppTitle from '@/components/app-title';
 import { DataGrid, useTableResolver } from '@/components/data-grid';
-import SkeletonDataGrid from '@/components/skeleton/skeleton-data-grid';
 import { Button } from '@/components/ui/button';
 import { useBreadcrumb } from '@/hooks/use-breadcrumb';
+import { usePermission } from '@/hooks/use-permissions';
 import AppLayout from '@/layouts/app-layout';
 import { SharedData } from '@/types';
 import { AssetAssignment, ResponseCollection } from '@/types/model';
 import { TableServer } from '@/types/table';
-import { Page, router } from '@inertiajs/core';
-import { usePage, WhenVisible } from '@inertiajs/react';
+import { router } from '@inertiajs/core';
+import { usePage } from '@inertiajs/react';
 import { PackagePlus } from 'lucide-react';
-import { useState } from 'react';
 import { columns as TableColumns } from './column';
 
 const AssetAssignmentListPage = () => {
-    const { component } = usePage<SharedData>();
+    const {
+        component,
+        props: { assignments, table },
+    } = usePage<SharedData & { assignments: ResponseCollection<AssetAssignment>; table: TableServer<AssetAssignment> }>();
     const breadcrumbs = useBreadcrumb(component);
-    const [assignments, setAssignments] = useState<ResponseCollection<AssetAssignment> | undefined>(undefined);
-    const { setTable, columns, tableState, tableStateServer } = useTableResolver('assets-assignment-table', TableColumns);
+    const { columns, tableState } = useTableResolver(table, TableColumns);
+    const { can } = usePermission();
 
     const handleReturn = (row: AssetAssignment) => {
         router.visit(route('asset-assignment.return', { reference_code: row.reference_code }));
@@ -39,37 +41,26 @@ const AssetAssignmentListPage = () => {
                     }
                 />
 
-                <WhenVisible
-                    params={{
-                        method: 'get',
-                        data: {
-                            ...(tableState ? tableStateServer : {}),
-                        },
-                        only: ['assetAssignments', 'table'],
-                        preserveUrl: true,
-                        async: true,
-                        onSuccess: (data) => {
-                            const typedData = data as Page<
-                                SharedData & { assetAssignments: ResponseCollection<AssetAssignment>; table: TableServer<AssetAssignment> }
-                            >;
-                            setAssignments(typedData.props.assetAssignments);
-                            setTable(typedData.props.table);
-                        },
-                    }}
-                    fallback={<SkeletonDataGrid />}
-                >
-                    <DataGrid
-                        columns={columns}
-                        rows={assignments?.data || []}
-                        pageSizeOptions={[10, 25, 50]}
-                        tableState={tableState}
-                        serverSide
-                        rowCount={assignments?.meta?.total || 0}
-                        rowId={(row) => (row.id ? row.id.toString() : '')}
-                        enableRowSelection
-                        actionsRow={(row) => [...(!row.returned_at ? [{ name: 'Return', event: handleReturn }] : [])]}
-                    />
-                </WhenVisible>
+                <DataGrid
+                    columns={columns}
+                    rows={assignments?.data || []}
+                    pageSizeOptions={[10, 25, 50]}
+                    tableState={tableState}
+                    serverSide
+                    rowCount={assignments?.meta?.total || 0}
+                    rowId={(row) => (row.id ? row.id.toString() : '')}
+                    enableRowSelection
+                    actionsRow={() => [
+                        ...(can('asset_return.create')
+                            ? [
+                                  {
+                                      name: 'Return',
+                                      event: handleReturn,
+                                  },
+                              ]
+                            : []),
+                    ]}
+                />
             </AppContainer>
         </AppLayout>
     );
