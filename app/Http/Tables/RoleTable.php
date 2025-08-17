66bd0ee685c6f4871392a147c09571241151db0b
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-17 14:12:21 +0700

    feat: create RoleTable

---
+++ app/Http/Tables/RoleTable.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Tables;
+
+use App\Models\Role;
+use App\Tables\Columns\Column;
+use App\Tables\FilterColumns\DateFilterColumn;
+use App\Tables\NewTable;
+use Illuminate\Database\Eloquent\Builder;
+
+class RoleTable extends NewTable
+{
+    protected string $name = 'roles';
+
+    protected string $rowId = 'id';
+
+    public function resource(): Builder|string
+    {
+        return Role::class;
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make('name')
+                ->label('Name')
+                ->globallySearchable()
+                ->sortable()
+                ->toggleable(),
+            Column::count('permissions', 'id')
+                ->label('Permissions')
+                ->searchableCount()
+                ->sortableCount()
+                ->toggleable(),
+            Column::make('created_at')
+                ->label('Created At')
+                ->globallySearchable()
+                ->sortable()
+                ->toggleable(),
+        ];
+    }
+
+    public function filters(): array
+    {
+        return [
+            DateFilterColumn::make('created_at')
+                ->label('Created At'),
+        ];
+    }
+
+    public function defaultSort(): array
+    {
+        return ['-created_at'];
+    }
+}
