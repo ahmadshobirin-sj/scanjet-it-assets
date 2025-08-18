9d73457bb054f09829a5049647f7ffa4eb613e0a
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-18 09:17:32 +0700

    feat: create ManufactureTable

---
+++ app/Http/Tables/ManufactureTable.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Tables;
+
+use App\Models\Manufacture;
+use App\Tables\Columns\Column;
+use App\Tables\NewTable;
+use Illuminate\Database\Eloquent\Builder;
+
+class ManufactureTable extends NewTable
+{
+    public function resource(): Builder|string
+    {
+        return Manufacture::class;
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make('name')
+                ->label('Name')
+                ->sortable()
+                ->globallySearchable()
+                ->toggleable(),
+            Column::make('created_at')
+                ->label('Created At')
+                ->sortable()
+                ->globallySearchable()
+                ->toggleable(),
+        ];
+    }
+
+    public function filters(): array
+    {
+        return [];
+    }
+
+    public function defaultSort(): array
+    {
+        return ['-created_at'];
+    }
+}
