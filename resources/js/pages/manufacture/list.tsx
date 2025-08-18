3bd62447874062d12ef7c609cb9a827aac04f4ec
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-18 09:18:18 +0700

    feat: using ManufactureTable

--- resources/js/pages/manufacture/list.tsx
+++ resources/js/pages/manufacture/list.tsx
@@ -1,44 +1,24 @@
 import AppContainer from '@/components/app-container';
 import AppTitle from '@/components/app-title';
-import { DataGrid, DataGridState } from '@/components/data-grid';
+import { DataTable, DataTableResource } from '@/components/data-table';
 import { Button } from '@/components/ui/button';
 import { useBreadcrumb } from '@/hooks/use-breadcrumb';
-import useDidUpdate from '@/hooks/use-did-update';
 import { usePermission } from '@/hooks/use-permissions';
 import AppLayout from '@/layouts/app-layout';
 import { confirmDialog } from '@/lib/confirmDialog';
-import { spatieToTanstackState, tanstackToSpatieParams } from '@/lib/normalize-table-state';
+import { formatWithBrowserTimezone } from '@/lib/date';
 import { SharedData } from '@/types';
-import { Manufacture, ResponseCollection, TableServerState } from '@/types/model';
+import { Manufacture } from '@/types/model';
 import { router, usePage } from '@inertiajs/react';
-import { PaginationState, SortingState } from '@tanstack/react-table';
 import { Plus } from 'lucide-react';
-import { useCallback, useEffect, useState } from 'react';
-import { toast } from 'sonner';
-import { columns } from './column';
 
 const ManufactureListPage = () => {
     const {
-        props: { manufactures, table, success, errors },
+        props: { manufactures },
         component,
-    } = usePage<SharedData & { manufactures: ResponseCollection<Manufacture>; table: TableServerState }>();
-    const [tableState, setTableState] = useState(spatieToTanstackState(table));
+    } = usePage<SharedData & { manufactures: DataTableResource<Manufacture> }>();
     const breadcrumbs = useBreadcrumb(component);
     const { can } = usePermission();
-
-    // Toast notification for success or error messages
-    useEffect(() => {
-        if (success) {
-            toast.success((success as any).message as any);
-        }
-        if (errors) {
-            if (errors.message) {
-                toast.error(errors.message, {
-                    ...(errors.error ? { description: errors.error } : {}),
-                });
-            }
-        }
-    }, [success, errors]);
 
     const handleView = (row: Manufacture) => {
         router.visit(route('manufacture.show', { manufacture: row.id }));
@@ -66,46 +46,6 @@ const ManufactureListPage = () => {
         });
     };
 
-    const updateTableState = useCallback((tableState: DataGridState) => {
-        const query = tanstackToSpatieParams(tableState);
-        router.get(route('manufacture.index'), query, {
-            preserveState: true,
-            preserveScroll: true,
-        });
-    }, []);
-
-    useDidUpdate(() => {
-        updateTableState(tableState);
-    }, [tableState]);
-
-    const handlePaginationChange = useCallback(
-        (pagination: PaginationState) => {
-            setTableState((prev) => ({ ...prev, pagination }));
-        },
-        [setTableState],
-    );
-
-    const handleSortingChange = useCallback(
-        (sorting: SortingState) => {
-            setTableState((prev) => ({ ...prev, sorting }));
-        },
-        [setTableState],
-    );
-
-    const handleFilterChange = useCallback(
-        (globalFilter: string) => {
-            setTableState((prev) => ({
-                ...prev,
-                globalFilter,
-                pagination: {
-                    ...prev.pagination,
-                    pageIndex: 0,
-                },
-            }));
-        },
-        [setTableState],
-    );
-
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <AppContainer className="space-y-6">
@@ -122,24 +62,21 @@ const ManufactureListPage = () => {
                         </>
                     }
                 />
-                <DataGrid
-                    rows={manufactures?.data || []}
-                    columns={columns}
-                    tableState={tableState}
-                    pageSizeOptions={[10, 25, 50]}
-                    rowCount={manufactures?.meta?.total || 0}
-                    enableRowSelection={false}
-                    rowId={(row) => row.id.toString()}
-                    serverSide={true}
-                    onSortingChange={handleSortingChange}
-                    onGlobalFilterChange={handleFilterChange}
-                    onPaginationChange={handlePaginationChange}
+                <DataTable
+                    resource={manufactures}
+                    transformerColumns={{
+                        created_at: (column) => ({
+                            ...column,
+                            cell: ({ row }) => formatWithBrowserTimezone(row.original.created_at),
+                        }),
+                    }}
+                    bulkActions={[]}
+                    exportActions={[]}
                     actionsRow={() => [
                         ...(can('manufacture.view') ? [{ name: 'View', event: handleView }] : []),
                         ...(can('manufacture.update') ? [{ name: 'Edit', event: handleEdit }] : []),
                         ...(can('manufacture.delete') ? [{ name: 'Delete', color: 'destructive', event: handleDelete }] : []),
                     ]}
-                    emptyText="No data manufactures available"
                 />
             </AppContainer>
         </AppLayout>
