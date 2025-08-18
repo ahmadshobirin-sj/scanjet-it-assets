cc51d716e9deb55e3154f81379da3525f345636a
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-18 08:11:41 +0700

    feat: create component AppToast

---
+++ resources/js/components/toast.tsx
@@ -0,0 +1,10 @@
+import { AppNotificationItem, AppNotificationItemProps } from '@/components/app-notification/app-notification-item';
+import { toast as sonnerToast } from 'sonner';
+
+type ToastProps = Omit<AppNotificationItemProps, 'keyToast' | 'toast'>
+
+const AppToast = (props: ToastProps) => {
+    return sonnerToast.custom((id) => <AppNotificationItem keyToast={id} toast={sonnerToast} {...props} />);
+};
+
+export default AppToast;
