d405225b8b931070cffd7ff86187136bb64096c1
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-18 09:18:32 +0700

    feat: using ManufactureTable

--- app/Http/Controllers/ManufactureController.php
+++ app/Http/Controllers/ManufactureController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\DTOs\TableStateDTO;
 use App\Http\Requests\Manufacture\ManufactureStoreRequest;
 use App\Http\Requests\Manufacture\ManufactureUpdateRequest;
 use App\Http\Resources\ManufactureResource;
 use App\Http\Services\ManufactureService;
+use App\Http\Tables\ManufactureTable;
 use App\Models\Manufacture;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
@@ -22,16 +22,20 @@ class ManufactureController extends Controller
     {
         $this->authorize('viewAny', Manufacture::class);
 
-        $manufactures = ManufactureResource::collection(
-            $this->manufactureService->getAll($request)
-        );
+        $manufactures = [];
 
-        $tableState = TableStateDTO::fromRequest($request);
-        $tableState->setSort(['-created_at']);
+        try {
+            $manufactures = ManufactureTable::make('manufacture')->toSchema();
+        } catch (\Throwable $e) {
+            if (app()->isProduction()) {
+                report($e);
+            } else {
+                throw $e;
+            }
+        }
 
         return Inertia::render('manufacture/list', [
             'manufactures' => $manufactures,
-            'table' => $tableState->toArray(),
         ]);
     }
 
