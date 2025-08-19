018eeba6b303e65ba448275ab0a52c650a592c34
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-19 10:12:48 +0700

    feat: support local ssl

--- config/reverb.php
+++ config/reverb.php
@@ -34,7 +34,11 @@ return [
             'path' => env('REVERB_SERVER_PATH', ''),
             'hostname' => env('REVERB_HOST'),
             'options' => [
-                'tls' => [],
+                'tls' => [
+                    'verify_peer' => env('APP_ENV') !== 'local',
+                    'local_cert' => env('LOCAL_CERTIFICATE_PATH'),
+                    'local_pk' => env('LOCAL_CERTIFICATE_KEY_PATH'),
+                ],
             ],
             'max_request_size' => env('REVERB_MAX_REQUEST_SIZE', 10_000),
             'scaling' => [
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,49 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 import laravel from 'laravel-vite-plugin';
 import { resolve } from 'node:path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            ssr: 'resources/js/ssr.tsx',
-            refresh: true,
-        }),
-        react(),
-        tailwindcss(),
-    ],
-    esbuild: {
-        jsx: 'automatic',
-    },
-    resolve: {
-        alias: {
-            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    const hasHTTPS =
+        env.LOCAL_CERTIFICATE_KEY_PATH &&
+        env.LOCAL_CERTIFICATE_PATH &&
+        fs.existsSync(env.LOCAL_CERTIFICATE_KEY_PATH) &&
+        fs.existsSync(env.LOCAL_CERTIFICATE_PATH);
+
+    return {
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.tsx'],
+                ssr: 'resources/js/ssr.tsx',
+                refresh: true,
+            }),
+            react(),
+            tailwindcss(),
+        ],
+        esbuild: {
+            jsx: 'automatic',
         },
-    },
+        resolve: {
+            alias: {
+                'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+            },
+        },
+        server: {
+            host: '127.0.0.1',
+            port: 5173,
+            https: hasHTTPS
+                ? {
+                      key: fs.readFileSync(env.LOCAL_CERTIFICATE_KEY_PATH),
+                      cert: fs.readFileSync(env.LOCAL_CERTIFICATE_PATH),
+                  }
+                : {},
+            hmr: {
+                host: 'scanjet.local',
+            },
+        },
+    };
 });
