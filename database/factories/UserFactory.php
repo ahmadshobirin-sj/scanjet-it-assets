4e96715d8ba8e3cab8f7a7aac99b39b68d7517ae
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-20 10:29:54 +0700

    feat: update user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,14 +3,17 @@
 namespace Database\Factories;
 
 use App\Enums\UserRole;
-use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Spatie\Permission\Models\Role;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
+    protected $model = User::class;
+
     /**
      * Define the model's default state.
      *
@@ -21,14 +24,21 @@ class UserFactory extends Factory
         return [
             'name' => fake()->unique()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'password' => bcrypt('password'), // jangan lupa password default
         ];
     }
 
     public function configure()
     {
         return $this->afterCreating(function ($user) {
-            $role = Role::firstOrCreate(['name' => UserRole::INACTIVE->value]);
-            $user->assignRole($role);
+            // default assign role Inactive
+            $role = Role::where('name', UserRole::INACTIVE->value)
+                ->where('guard_name', config('auth.defaults.guard', 'web'))
+                ->first();
+
+            if ($role) {
+                $user->assignRole($role);
+            }
         });
     }
 
@@ -36,8 +46,13 @@ class UserFactory extends Factory
     public function superAdmin()
     {
         return $this->afterCreating(function ($user) {
-            $role = Role::firstOrCreate(['name' => UserRole::SUPER_ADMIN->value]);
-            $user->assignRole($role);
+            $role = Role::where('name', UserRole::SUPER_ADMIN->value)
+                ->where('guard_name', config('auth.defaults.guard', 'web'))
+                ->first();
+
+            if ($role) {
+                $user->assignRole($role);
+            }
         });
     }
 
@@ -45,8 +60,13 @@ class UserFactory extends Factory
     public function admin()
     {
         return $this->afterCreating(function ($user) {
-            $role = Role::firstOrCreate(['name' => UserRole::ADMIN->value]);
-            $user->assignRole($role);
+            $role = Role::where('name', UserRole::ADMIN->value)
+                ->where('guard_name', config('auth.defaults.guard', 'web'))
+                ->first();
+
+            if ($role) {
+                $user->assignRole($role);
+            }
         });
     }
 }
