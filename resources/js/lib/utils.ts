10f816c96617c8fcccbb015bdc9fad0e6794bb5e
Ahmad Shobirin <ahmad.shobirin@scanjet.net>
2025-08-28 10:37:13 +0700

    feat: utils for format bytes

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -22,3 +22,21 @@ export function camelCaseToWords(str: string, separator = ' ') {
             .toLowerCase()
     );
 }
+
+export function formatBytes(bytes: number, decimals = 2): string {
+    if (bytes === 0) return '0 B';
+
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    const value = parseFloat((bytes / Math.pow(k, i)).toFixed(dm));
+
+    return `${value} ${sizes[i]}`;
+}
+
+export function progressPercent(loaded?: number, total?: number) {
+    if (!loaded || !total || total === 0) return 0; // total bisa undefined di beberapa server
+    return Math.min(100, Math.round((loaded / total) * 100));
+}
